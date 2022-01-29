1448facec38099984ef5a0c41422c9de4dd4d869
Luiz Gustavo Santos Farias <lgsfarias.dev@gmail.com>
2022-01-29 08:41:45 -0300

    Versão 1.0 DrivenEats

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// declaração das variáveis
 let prato
 let bebida
 let sobremesa
@@ -11,6 +12,9 @@ let nome
 let endereco
 
 function escolhePrato (i) {
+    // Esta função é invocada quando o usuário clica em uma opção de prato principal.
+    // Ela tira a seleção dos outros pratos disponiveis e seleciona o prato escolhido.
+    // Além de guardar nas variáveis prato e precoPrato o nome e o valor do prato, respectivamente, e os insere no PopUp de confirmação.
     prato = i.children[1].innerText
     precoPrato = i.children[3].innerText.slice(3);
     for (opcao of i.parentElement.children){
@@ -20,18 +24,12 @@ function escolhePrato (i) {
     i.children[4].style.display="block";
     i.style.borderColor="#32B72F";
     document.querySelector(".popupPrato").innerHTML=prato+" <span>"+precoPrato+"</span>";
-    selecao()
-
-    // document.getElementById("frango").style.borderColor="transparent";
-    // document.getElementById("carne").style.borderColor="transparent";
-    // document.getElementById("misto").style.borderColor="transparent";
-
-    // document.getElementById("frango").children[4].style.display="none";
-    // document.getElementById("carne").children[4].style.display="none";
-    // document.getElementById("misto").children[4].style.display="none";
-    
+    verificacao()
 }
 function escolheBebida (i) {
+    // Esta função é invocada quando o usuário clica em uma opção de bebida.
+    // Ela tira a seleção das outras bebidas disponiveis e seleciona a bebida escolhida.
+    // Além de guardar nas variáveis bebida e precoBebida o nome e o valor da bebida, respectivamente, e os insere no PopUp de confirmação.
     bebida = i.children[1].innerText
     precoBebida = i.children[3].innerText.slice(3);
     for (opcao of i.parentElement.children){
@@ -41,9 +39,12 @@ function escolheBebida (i) {
     i.children[4].style.display="block";
     i.style.borderColor="#32B72F";
     document.querySelector(".popupBebida").innerHTML=bebida+" <span>"+precoBebida+"</span>";
-    selecao()
+    verificacao()
 }
 function escolheSobremesa (i) {
+    // Esta função é invocada quando o usuário clica em uma opção de sobremesa.
+    // Ela tira a seleção das outras sobremesas disponiveis e seleciona a sobremesa escolhida.
+    // Além de guardar nas variáveis sobremesa e precoSobremesa o nome e o valor da sobremesa, respectivamente, e os insere no PopUp de confirmação.
     sobremesa = i.children[1].innerText;
     precoSobremesa = i.children[3].innerText.slice(3);
     for (opcao of i.parentElement.children){
@@ -53,17 +54,22 @@ function escolheSobremesa (i) {
     i.children[4].style.display="block";
     i.style.borderColor="#32B72F";
     document.querySelector(".popupSobremesa").innerHTML=sobremesa+" <span>"+precoSobremesa+"</span>";
-    selecao()
+    verificacao()
 }
-function selecao () {
+function verificacao () {
+    // Esta função é invocada toda vez que o usuário seleciona uma opção.
+    // Ela verifica se as três opções ja foram escolhidas. 
+    // Se sim, ela habilita o botão fechar pedido.
     if (prato!==undefined && bebida!== undefined && sobremesa!==undefined){
         document.getElementById("finaliza").style.backgroundColor="#32B72F";
         document.getElementById("finaliza").style.color="white";
         document.getElementById("finaliza").innerText="Fechar pedido";
     }
-    // console.log(prato +"     "+ precoPrato + "\n"+ bebida+"     "+precoBebida+ "\n"+ sobremesa+"     "+precoSobremesa);
 }
 function finaliza () {
+    // Esta função é invocada quando o usuário clica no botão "fechar pedido".
+    // Apenas se as 3 opções ja estiverem sido escolhidas, ela pede, através do prompt, o nome e o endereço do usuário. 
+    // Ela também calcula o total do pedido e insere no PopUp de confirmação.
     if (prato!==undefined && bebida!== undefined && sobremesa!==undefined){
         if (nome===undefined){
             nome = prompt("Informe seu nome completo, por favor!")
@@ -77,18 +83,15 @@ function finaliza () {
     }
 }
 function cancelar () {
+    // Esta função é invocada quando o usuário clica no botão "Cancelar".
+    // Ela fecha o PopUp de confirmação para que o usuário possa refazer o pedido.
     document.querySelector(".popup-wrapper").classList.add("escondido");
 }
 function tudoCerto () {
+    // Esta função é invocada quando o usuário clica no botão "Tudo certo, pode pedir!".
+    // Ela elabora uma mensagem com todas as informções do pedido em envia por whatsapp para a equipe do restaurante..
     let mensagem;
-    mensagem = `Olá, gostaria de fazer o pedido:\n
-- Prato: ${prato}\n
-- Bebida: ${bebida}\n
-- Sobremesa: ${sobremesa}\n
-Total: R$ ${total}\n
-\n
-Nome: ${nome}\n
-Endereço: ${endereco}`
+    mensagem = `Olá, gostaria de fazer o pedido:\n- Prato: ${prato}\n- Bebida: ${bebida}\n- Sobremesa: ${sobremesa}\nTotal: R$ ${total}\n\nNome: ${nome}\nEndereço: ${endereco}`
     console.log(mensagem);
-    window.open("https://wa.me/+5521979429298?text="+encodeURIComponent(mensagem));
+    window.open("https://wa.me/+5521980399582?text="+encodeURIComponent(mensagem));
 }
